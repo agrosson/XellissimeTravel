96ba598141074b1ff3e549fc429b1903f3494037
agrosson <agrosson@gmail.com>
2019-03-26 16:37:39 +0100

    Create first screen with button shows tab bar

---
+++ XellissimeTravel/Controller/InitialScreenViewController.swift
@@ -0,0 +1,34 @@
+//
+//  InitialScreenViewController.swift
+//  XellissimeTravel
+//
+//  Created by ALEXANDRE GROSSON on 26/03/2019.
+//  Copyright © 2019 GROSSON. All rights reserved.
+//
+
+import UIKit
+
+class InitialScreenViewController: UIViewController {
+    @IBAction func unwindToFirst(segue: UIStoryboardSegue){
+    }
+    
+    @IBAction func goToNextButton(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
