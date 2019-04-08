e46b879af187b83c178380e574a4e869a27e85e7
agrosson <agrosson@gmail.com>
2019-04-08 09:34:31 +0200

    Update parameters with enum and switch

--- XellissimeTravel/Controller/ParamtersViewController.swift
+++ XellissimeTravel/Controller/ParamtersViewController.swift
@@ -15,9 +15,42 @@ class ParamtersViewController: UIViewController {
     @IBOutlet weak var parametersView3: UIView!
     @IBOutlet weak var parametersView4: UIView!
     
+    var color1: UIColor?
+    var color2: UIColor?
+    var color3: UIColor?
+    var color4: UIColor?
     
+    enum Style {
+            case style1, style2, style3, style4
+    }
+    
+    var style : Style = .style1 {
+        didSet{
+            setStyle(style)
+        }
+    }
+    private func setStyle(_ style: Style) {
+        switch style {
+        case .style1:
+            print("style1")
+        case .style2:
+            print("style2")
+        case .style3:
+            print("style3")
+        case .style4:
+            print("style4")
+        }
+        
+    }
     
     @IBAction func paramtersSegmentedSelected(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case 0: style = .style1
+            case 1: style = .style2
+            case 2: style = .style3
+            case 3: style = .style4
+        default : style = .style1
+        }
     }
     
     override func viewDidLoad() {
@@ -26,15 +59,11 @@ class ParamtersViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func redMood() {
+        parametersView1.backgroundColor = UIColor(hexString: "E13932")
+        parametersView2.backgroundColor = UIColor(hexString: "881313")
+        parametersView3.backgroundColor = UIColor(hexString: "FC3B10")
+        parametersView4.backgroundColor = UIColor(hexString: "ef5b17")
     }
-    */
-
 }
+
---
+++ XellissimeTravel/Model/ExtensionNotificationName.swift
@@ -0,0 +1,16 @@
+//
+//  ExtensionNotificationName.swift
+//  
+//
+//  Created by ALEXANDRE GROSSON on 08/04/2019.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static let setNewColor1 = Notification.Name(rawValue: "setNewColor1")
+    static let setNewColor2 = Notification.Name(rawValue: "setNewColor2")
+    static let setNewColor3 = Notification.Name(rawValue: "setNewColor3")
+    static let setNewColor4 = Notification.Name(rawValue: "setNewColor4")
+}
