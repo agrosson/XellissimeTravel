abc7f0c509b2d945d6bfcde7aa40caaf615e046e
agrosson <agrosson@gmail.com>
2019-04-09 14:49:13 +0200

    update .ds store

---
+++ ExtensionNotificationName.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionNotificationName.swift
+//  
+//
+//  Created by ALEXANDRE GROSSON on 08/04/2019.
+//
+
+import Foundation
+
+
+extension Notification.Name {
+    static let setNewColor1 = Notification.Name(rawValue: "setNewColor1")
+}
