f8229cd8a562f2f157a072afd40c04f01c80d5d0
agrosson <agrosson@gmail.com>
2019-04-09 11:33:05 +0200

    Add comments in ChangeScreen1VC

--- XellissimeTravel/Controller/ChangeScreen1ViewController.swift
+++ XellissimeTravel/Controller/ChangeScreen1ViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class ChangeScreen1ViewController: UIViewController {
     
+    // MARK: - Outlets - Label
+     @IBOutlet weak var introLabel: UILabel!
+    // MARK: - Outlets - Button
     @IBOutlet weak var goLabel: UIButton!
-    @IBOutlet weak var introLabel: UILabel!
+   // MARK: - Actions
     @IBAction func changeGoButton(_ sender: UIButton) {
-
     }
-    
+     // MARK: - Methods - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = color2
@@ -24,17 +26,19 @@ class ChangeScreen1ViewController: UIViewController {
         navigationBarColor()
         NotificationCenter.default.addObserver(self, selector: #selector(updateColor), name: .setNewColor1, object: nil)
     }
-    
+    // MARK: - Methods - ViewDidAppear
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         NotificationCenter.default.addObserver(self, selector: #selector(updateColor), name: .setNewColor1, object: nil)
     }
+    // MARK: - Methods
+    /**
+     Function to update colors of screen, listening to Notification sent from parameters options
+     */
     @objc func updateColor(notification : Notification){
-
         view.backgroundColor = color2
         self.introLabel.textColor = .white
         goLabel.setTitleColor(.white, for: .normal)
         navigationBarColor()
-        
     }
 }
